7b6a94d9e2f713fa078e650f71bdaa831ba5f081
Mohammad Jewel <jewelmm9@gmail.com>
2025-04-21 22:57:42 +0600

    svg animation by gsap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,54 @@
 gsap.to("#box", {
   duration: 2,
   x: 300,
-  //rotation: 360,
-  //scale: 0.5,
+  y: 200,
+  rotation: 360,
+  scale: 0.5,
   borderRadius: "20%",
   ease: "bounce.out",
   repeat: -1,
   yoyo: true,
 });
-gsap.to("#box2", {
+
+gsap.from(".section1 .box", {
+  scale: 0,
   duration: 2,
-  x: 300,
-  //rotation: 360,
-  //scale: 0.5,
-  borderRadius: "20%",
-  ease: "bounce.out",
-  repeat: -1,
-  yoyo: true,
-  delay: 2,
+  delay: 0.5,
+  rotation: 360,
 });
-gsap.from("h1", {
+gsap.from(".section2 .box", {
+  scale: 0,
   duration: 2,
-  y: 30,
-  repeat: -1,
-  ease: "easeOut",
-  //yoyo: true,
-  opacity: 0,
-  delay: 1,
-  stagger: 2,
+  delay: 0.5,
+  rotation: 360,
+  scrollTrigger: {
+    trigger: ".section2 .box",
+    scroller: "body",
+    markers: true,
+    start: "top 80%",
+    end: "top 30%",
+  },
+});
+// line animation
+
+//let path = `M 10 100 Q 500 100 990 100`;
+let finalPath = `M 10 100 Q 500 100 990 100`;
+
+let string = document.querySelector(".string");
+
+string.addEventListener("mousemove", function (dets) {
+  path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`;
+  gsap.to("svg path", {
+    duration: 0.3,
+    attr: { d: path },
+    ease: "power3.out",
+  });
+});
+
+string.addEventListener("mouseleave", function () {
+  gsap.to("svg path", {
+    duration: 1.5,
+    attr: { d: finalPath },
+    ease: "elastic.out(1, 0.3)",
+  });
 });
